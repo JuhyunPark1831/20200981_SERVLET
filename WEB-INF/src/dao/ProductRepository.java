c1dc6c28a522cf13300953c62604325f70402575
박주현 <epahs1831@sungkyul.ac.kr>
2022-09-24 18:28:59 +0000

    .

--- WEB-INF/src/dao/ProductRepository.java
+++ WEB-INF/src/dao/ProductRepository.java
@@ -13,25 +13,73 @@ public class ProductRepository {
 		phone.setManufacturer("Apple");
 		phone.setUnitsInStock(1000);
 		phone.setCondition("New");
-        		Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+        
+        Product notebook = new Product("P1235", "LG PC 그램", 1500000);
 		notebook.setDescription("13.3-inch, IPS LED display, 5rd Generation Intel Core processors");
 		notebook.setCategory("Notebook");
 		notebook.setManufacturer("LG");
 		notebook.setUnitsInStock(1000);
 		notebook.setCondition("Refurbished");
-
-		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+        
+        Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
 		tablet.setDescription("212.8*125.6*6.6mm,  Super AMOLED display, Octa-Core processor");
 		tablet.setCategory("Tablet");
 		tablet.setManufacturer("Samsung");
 		tablet.setUnitsInStock(1000);
 		tablet.setCondition("Old");
-		
+        
+        Product TV = new Product("P1237", "UHD 189 cm", 1400000);
+        TV.setDescription("189 cm, Crystal Processor 4K, HDR");
+		TV.setCategory("TV");
+		TV.setManufacturer("Samsung");
+		TV.setUnitsInStock(1000);
+		TV.setCondition("New");
+        
+        Product headset = new Product("P1238", "YH G01", 200000);
+        headset.setDescription("27mm, 20Hz ~ 20kHz, electronic training");
+		headset.setCategory("Headset");
+		headset.setManufacturer("YAMAHA");
+		headset.setUnitsInStock(1000);
+		headset.setCondition("New");
+        
+        Product mouse = new Product("P1239", "G502 X", 100000);
+        mouse.setDescription("131.4mm x 41.1mm x 79.2mm");
+		mouse.setCategory("Mouse");
+		mouse.setManufacturer("Logitech");
+		mouse.setUnitsInStock(1000);
+		mouse.setCondition("New");
+        
+        Product keyboard = new Product("P1240", "VALKAN gaming keyboard", 70000);
+        keyboard.setDescription("138mm x 448mm x 39mm");
+		keyboard.setCategory("Keyboard");
+		keyboard.setManufacturer("TNS INTERNATIONAL");
+		keyboard.setUnitsInStock(1000);
+		keyboard.setCondition("New");
+        
+        Product GPU = new Product("P1241", "MSI GEFORCE GTX 1660 SUPER", 300000);
+        GPU.setDescription("6GB, Express x 63.0, DisplayPort x 3(v1.4)/HDMI 2.0b x1");
+		GPU.setCategory("GPU");
+		GPU.setManufacturer("MSI");
+		GPU.setUnitsInStock(1000);
+		GPU.setCondition("New");
+        
+        Product monitor = new Product("P1242", "24MK430H", 200000);
+        monitor.setDescription("24inch");
+		monitor.setCategory("Monitor");
+		monitor.setManufacturer("LG");
+		monitor.setUnitsInStock(1000);
+		monitor.setCondition("New");
 		// 위와 같이 상품 초기화 하고 아래에 상품을 추가
 
 		listOfProducts.add(phone);
 		listOfProducts.add(notebook);
 		listOfProducts.add(tablet);
+        listOfProducts.add(TV);
+        listOfProducts.add(headset);
+        listOfProducts.add(mouse);
+        listOfProducts.add(keyboard);
+        listOfProducts.add(GPU);
+        listOfProducts.add(monitor);
 		// listOfProducts.add(상품명);
 	}
 
--- index.jsp
+++ index.jsp
@@ -1,6 +1,8 @@
 <%@ page contentType="text/html; charset=utf-8"%>
 <%@ page import="java.util.Date"%>
 
+
+
 <html>
 <head>
     <script>
@@ -16,5 +18,8 @@
 	<%@ include file="top_menu.jsp" %>
 	<%@ include file="body_main.jsp" %>	
     <%@ include file="footer.jsp" %>
+     현재 페이지 버퍼 용량 : <%= out.getBufferSize() %> <br>
+ 남은 페이지 버퍼 용량 : <%= out.getRemaining() %>
+
 </body>
 </html>
